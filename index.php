6902498324941afde72e263948d971f448dfffc6
Shogo Esaki @note01 <esaki28@gmail.com>
2018-12-02 23:01:58 +0900

    array

--- index.php
+++ index.php
@@ -4,7 +4,19 @@
 <meta charset="utf-8">
 <title>ホームページ練習</title>
 </head>
-	<p>他の授業のphpファイルを持ってきました</p>
+	<p>他の授業で作成しているphpファイルを持ってきました</p>
+	<form action"index.php" method="get">
+		<input type="text" name="nbr">
+		<input type="submit" value="照会">
+	</form>
+	<br>
+	<?php>
+		$nagaya = array(101,102,103,104,105,106);
+	<?>
+	<?php>
+		
+	<?>
+	
 <body>
 </body>
 </html>
